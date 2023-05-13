3c1df48ce84ddd358f05e97485677499f3bc095b
梁益嘉 <2894627171@qq.com>
2023-05-13 17:13:54 +0800

    1.josn数据解析 2.josn数据动态显示在table中

--- src/main/java/cn/edu/guet/TicketServlet.java
+++ src/main/java/cn/edu/guet/TicketServlet.java
@@ -19,29 +19,18 @@ public class TicketServlet extends HttpServlet {
         /*
         响应JSON
          */
-//        response.setContentType("application/json;charset=UTF-8");
-//        String weather="{\"status\":\"1\",\"count\":\"1\",\"info\":\"OK\",\"infocode\":\"10000\",\"lives\":[{\"province\":\"北京\",\"city\":\"东城区\",\"adcode\":\"110101\",\"weather\":\"晴\",\"temperature\":\"24\",\"winddirection\":\"北\",\"windpower\":\"≤3\",\"humidity\":\"16\",\"reporttime\":\"2023-05-07 11:39:12\",\"temperature_float\":\"24.0\",\"humidity_float\":\"16.0\"}]}";
-//        PrintWriter out=response.getWriter();
-//        out.print(weather);
-//        out.flush();
-//        out.close();
-//        点击《查询》后，对后台发请求，那么form表单中的数据：起始站、终点站、出发日期，都被封装到了request中
         String startStation = request.getParameter("startStation");
         System.out.println("起始站：" + startStation);
         String endStation = request.getParameter("endStation");
         System.out.println("终点站："+endStation);
-
         String departureDate=request.getParameter("departureDate");
         System.out.println("出发日期："+departureDate);
 
-//        List<Ticket> ticketList = TicketSearch.search(startStation, endStation, departureDate);
         String ticketList = TicketSearch.search(startStation, endStation, departureDate);
-
-        String json=JSON.toJSONString(ticketList, SerializerFeature.PrettyFormat);
 
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out=response.getWriter();
-        out.print(json);
+        out.print(ticketList);
         out.flush();
         out.close();
     }
